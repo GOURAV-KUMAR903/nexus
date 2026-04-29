b71a9c7009665548f6ff7b830b893711f324a1ec
Gourav Kumar <gni.gourav@gmail.com>
2026-04-29 11:44:11 +0530

    sdsad

--- database/db.py
+++ database/db.py
@@ -2,7 +2,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:@localhost/python"
+# DATABASE_URL = "mysql+pymysql://root:@localhost/python"
+DATABASE_URL = "postgresql://nexus_qk8j_user:GspjKn5Rk7P1WopYcFUWliyA6OPSsHsu@dpg-d7optnsm0tmc73d9o2ng-a.virginia-postgres.render.com/nexus_qk8j"
+
 
 engine = create_engine(DATABASE_URL, echo=True)  # echo=True optional, queries print karega
 
