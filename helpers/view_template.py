0f9e6734fa51051d564361f73c15bc27091c1d74
Gourav Kumar <gni.gourav@gmail.com>
2026-04-30 12:24:44 +0530

    hjvjbkj

--- helpers/view_template.py
+++ helpers/view_template.py
@@ -5,7 +5,11 @@ import constants   # <-- ye line add karo
 
 # Initialize templates folder
 templates = Jinja2Templates(directory="templates")
-templates.env.globals.update(vars(constants))
+templates.env.globals.update({
+    "APP_NAME": getattr(constants, "APP_NAME", None),
+    "VERSION": getattr(constants, "VERSION", None),
+    "CURRENCY": getattr(constants, "CURRENCY", None),
+})
 
 
 def render_view(template_name: str, request: Request, context: dict = None) -> HTMLResponse:
